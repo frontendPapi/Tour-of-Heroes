a604c7304a069d7360906da47613b2f2fa392695
Dejan Mihajlov <dejan.mihajlov@korvus.mk>
2023-02-28 15:52:02 +0100

    Project Done

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from '../hero';
+
+export const HEROES: Hero[] = [
+    {id: 12, name: 'Dejan'},
+    {id: 13, name: 'Aleksadar'},
+    {id: 14, name: 'Mile'},
+    {id: 15, name: 'Joco'},
+    {id: 16, name: 'Neno'},
+    {id: 17, name: 'Dimitar'},
+    {id: 18, name: 'Igor'},
+    {id: 19, name: 'Bojan'},
+    {id: 20, name: 'Vlatko'}
+]
